c90de768294282abd322a552a778673c04cae7ac
somnathb1 <snb895@outlook.com>
2025-08-04 03:22:44 +0400

    Cleanup

--- tests/perf/run_with_perf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-sudo perf stat --topdown -a -- taskset -c 0 build_gcc_release/cmd/rpcdaemon --target localhost:9090
--- tests/perf/run_with_toplev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-sudo toplev --core S0-C0 -l3 -v --no-desc taskset -c 0 build_gcc_release/cmd/rpcdaemon --target localhost:9090
