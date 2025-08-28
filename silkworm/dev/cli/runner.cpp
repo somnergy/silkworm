d1549a19eaed669cefb35be7609b2455eacd42dc
Paweł Bylica <pawel@ethereum.org>
2025-08-28 10:49:53 +0200

    state_transition: take args from CLI as in guest_program

--- silkworm/dev/cli/runner.cpp
+++ silkworm/dev/cli/runner.cpp
@@ -5,131 +5,34 @@
 
 // #include <CLI/CLI.hpp>
 
+#include <fstream>
+
 #include "../state_transition.hpp"
 
 // void execute_test(const std::string& json_str, bool terminate_flag, bool diagnostics_flag);
 using namespace silkworm::cmd::state_transition;
 
-int main() {
+int main(int argc, const char* argv[]) {
     try {
-        const std::string json_str = R"json(
-{
-    "1": {
-        "env": {
-            "baseFeePerGas": "0x0a",
-            "miner": "0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
-            "difficulty": "0x020000",
-            "gasLimit": "0x05f5e100",
-            "number": "0x01",
-            "currentRandom": "0x0000000000000000000000000000000000000000000000000000000000020000",
-            "timestamp": "0x03e8",
-            "parentHash": "0x5e20a0453cecd065ea59c37ac63e079ee08998b6045136a8ce6635c7912ec0b6"
-        },
-        "transactions": {
-            "Shanghai": [
-                {
-                    "data": "0x693c61390000000000000000000000000000000000000000000000000000000000000000",
-                    "gasLimit": "0x04c4b400",
-                    "gasPrice": "0x0a",
-                    "nonce": "0x00",
-                    "secretKey": "0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8",
-                    "sender": "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
-                    "to": "0xcccccccccccccccccccccccccccccccccccccccc",
-                    "value": "0x01"
-                },
-                {
-                    "data": "0x693c61390000000000000000000000000000000000000000000000000000000000000001",
-                    "gasLimit": "0x04c4b400",
-                    "gasPrice": "0x0a",
-                    "nonce": "0x00",
-                    "secretKey": "0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8",
-                    "sender": "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
-                    "to": "0xcccccccccccccccccccccccccccccccccccccccc",
-                    "value": "0x01"
-                },
-                {
-                    "data": "0x693c61390000000000000000000000000000000000000000000000000000000000000002",
-                    "gasLimit": "0x04c4b400",
-                    "gasPrice": "0x0a",
-                    "nonce": "0x00",
-                    "secretKey": "0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8",
-                    "sender": "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
-                    "to": "0xcccccccccccccccccccccccccccccccccccccccc",
-                    "value": "0x01"
-                },
-                {
-                    "data" : "0x693c61390000000000000000000000000000000000000000000000000000000000000400",
-                    "gasLimit" : "0x01a000",
-                    "gasPrice" : "0x0a",
-                    "nonce" : "0x00",
-                    "to" : "0xcccccccccccccccccccccccccccccccccccccccd",
-                    "value" : "0x0186a0"                    ,
-                    "sender" : "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b"
-                }
-            ]
-        },
-        "pre": {
-            "0x0000000000000000000000000000000000000100": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0x0000000000000000000000000000000000000101": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x60047fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0x0000000000000000000000000000000000000102": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x60017fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0x0000000000000000000000000000000000000103": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x600060000160005500",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0x0000000000000000000000000000000000000104": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60010160005500",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b": {
-                "balance": "0x3635c9adc5dea00000",
-                "code": "0x",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0xcccccccccccccccccccccccccccccccccccccccc": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x600060006000600060006004356101000162fffffff100",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0xcccccccccccccccccccccccccccccccccccccccd": {
-                "balance": "0x0ba1a9ce0ba1a9ce",
-                "code": "0x60d560243560043580801561054257806001146104b4578060021461042857806003146103c1578060041461034c57806005146102e55780600614610296578060071461022057806008146101aa57600914610142575b600f81116100a2575b5060008111610089575b60406102008360008060095af16000556102005160015561022051600255005b5b8015610069578060019160600181604401530361008a565b60ff907b48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f60005260008051602061056d8339815191526020526619cde05b61626360c81b60405260006060526000608052600060a0527b0300000000000000000000000000000001000000000000000000000060c05263ff000000811660181c60005362ff0000811660101c60015361ff00811660081c600253166003533861005f565b60008051602061054d83398151915260005260008051602061056d8339815191526020526819cde05b616263646560b81b60405260006060526000608052600060a0527b0500000000000000000000000000000001000000000000000000000060c052610056565b507bb736420d9819f695c458357b7a519844d4076b018d0c91c30ec9e2a01960005260008051602061056d8339815191526020526619cde05b61626360c81b60405260006060526000608052600060a0527b0300000000000000000000000000000001000000000000000000000060c052610056565b507c0148c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f60005260008051602061056d8339815191526020526619cde05b61626360c81b60405260006060526000608052600060a0527b0300000000000000000000000000000001000000000000000000000060c052610056565b5060008051602061054d833981519152600090815260008051602061056d8339815191526020526619cde05b61626360c81b6040526060819052608081905260a052600360d81b60c052610056565b5060008051602061054d83398151915260005260008051602061056d8339815191526020526619cde05b61626360c81b60405260006060526000608052600060a0527b0300000000000000000000000000000001000000000000000000000060c052610056565b507b48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f60005260008051602061056d8339815191526020526619cde05b61626360c81b60405260006060526000608052600060a0527b0300000000000000000000000000000001000000000000000000000060c052610056565b5060008051602061054d83398151915260005260008051602061056d8339815191526020526619cde05b61626360c81b60405260006060526000608052600060a0527b0300000000000000000000000000000002000000000000000000000060c052610056565b50915060d6917b0c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d6000527f5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e602052671319cde05b61626360c01b60405260006060526000608052600060a0527a03000000000000000000000000000000010000000000000000000060c052610056565b50915060d4917d0c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3a6000527ff54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e131960205265cde05b61626360d01b60405260006060526000608052600060a0527c030000000000000000000000000000000100000000000000000000000060c052610056565b506000925061005656fe0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e13",
-                "nonce": "0x00",
-                "storage": {}
-            },
-            "0x0000000000000000000000000000000000001000": {
-                "nonce": "0x01",
-                "balance": "0x00",
-                "code": "0x36600060003760006000366000600060055af16000557f601038036010600039601038036000f3000000000000000000000000000000006000523d600060103e3d60100160006000f000",
-                "storage": {}
-            }
+        if (argc < 3) {
+            std::cerr << "Usage: " << argv[0] << " <num_runs> <json_file_path>\n";
+            return 1;
         }
-    }
-}
-)json";
+
+        const uint32_t num_runs = std::stoul(argv[1]);
+        const std::string json_file_path = argv[2];
+        std::cout << "n: " << num_runs << "\n";
+
+        const std::string json_str = [](const std::string& path) {
+            std::ifstream file(path);
+            if (!file.is_open()) {
+                throw std::runtime_error("Could not open file: " + path);
+            }
+            return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        }(json_file_path);
         auto state_transition = StateTransition(json_str, false, true);
-        auto total_gas = state_transition.run(10);
-        std::cout << "Total Gas: " << total_gas << "\n";
+        auto total_gas = state_transition.run(num_runs);
+        std::cout << "Cumulative Gas Used: " << total_gas << "\n";
 
     } catch (const std::exception& e) {
         // code to handle exceptions of type std::exception and its derived classes
