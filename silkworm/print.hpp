f32e42753a453a423d4aed02754f5bc1ed19e9be
Paweł Bylica <pawel@ethereum.org>
2025-09-17 16:18:40 +0200

    add missing #include and syscall_halt

--- silkworm/print.hpp
+++ silkworm/print.hpp
@@ -3,10 +3,16 @@
 #ifdef SP1
 #include "sp1_syscalls.hpp"
 #else
+#include <iostream>
 inline void sys_println(const char* msg) {
     std::cout << "stdout: " << msg << std::endl;
 }
 inline void sys_print(const char* msg) {
     std::cout << "stdout: " << msg;
 }
+
+[[noreturn]] inline void syscall_halt(uint8_t exit_code) {
+    std::exit(exit_code);
+}
+
 #endif
