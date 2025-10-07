1c3c76f768d2c1003df1880a830939041fbd5e3a
Paweł Bylica <pawel@ethereum.org>
2025-10-07 20:01:57 +0200

    Use evmone's implementation of ecrecovery

--- silkworm/core/crypto/ecdsa.cpp
+++ silkworm/core/crypto/ecdsa.cpp
@@ -3,45 +3,19 @@
 
 #include "ecdsa.h"
 
-#include <evmone_precompiles/keccak.h>
-#include <secp256k1_recovery.h>
-#include <cstring>
-
-//! \brief Tries recover public key used for message signing.
-static bool recover(uint8_t public_key[65], const uint8_t message[32], const uint8_t signature[64], uint8_t recovery_id,
-                    const secp256k1_context* context) {
-    secp256k1_ecdsa_recoverable_signature sig;
-    if (!secp256k1_ecdsa_recoverable_signature_parse_compact(context, &sig, signature, recovery_id)) {
-        return false;
-    }
-
-    secp256k1_pubkey pub_key;
-    if (!secp256k1_ecdsa_recover(context, &pub_key, &sig, message)) {
-        return false;
-    }
-
-    size_t key_len = 65;
-    return secp256k1_ec_pubkey_serialize(context, public_key, &key_len, &pub_key, SECP256K1_EC_UNCOMPRESSED);
-}
-
-//! Tries extract address from recovered public key
-//! \param [in] public_key: The recovered public key
-//! \return Whether the recovery has succeeded.
-static bool public_key_to_address(uint8_t out[20], const uint8_t public_key[65]) {
-    if (public_key[0] != 4u) {
-        return false;
-    }
-    // Ignore first byte of public key
-    const union ethash_hash256 key_hash = ethash_keccak256(public_key + 1, 64);
-    std::memcpy(out, &key_hash.bytes[12], 20);
-    return true;
-}
+#include <evmone_precompiles/secp256k1.hpp>
 
 bool silkworm_recover_address(uint8_t out[20], const uint8_t message[32], const uint8_t signature[64],
                               uint8_t recovery_id) {
-    uint8_t public_key[65];
-    if (!recover(public_key, message, signature, recovery_id, secp256k1_context_static)) {
+    ethash::hash256 msg_hash;
+    std::memcpy(msg_hash.bytes, message, 32);
+    const auto opt_address = evmmax::secp256k1::ecrecover(msg_hash,
+                                                          intx::be::unsafe::load<intx::uint256>(&signature[0]),
+                                                          intx::be::unsafe::load<intx::uint256>(&signature[32]),
+                                                          recovery_id != 0);
+    if (!opt_address) {
         return false;
     }
-    return public_key_to_address(out, public_key);
+    std::memcpy(out, opt_address->bytes, 20);
+    return true;
 }
